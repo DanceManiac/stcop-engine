e54a87958a372224c7a663499590888087632b63
Mortan <cheatmaster1@mail.ru>
2021-03-04 11:46:12 +0300

    * OPT: чистка & обновление скриншотилки

--- src/Layers/xrRender/HW.h
+++ src/Layers/xrRender/HW.h
@@ -7,13 +7,8 @@
 #pragma once
 
 #include "hwcaps.h"
-
-
-#ifndef _MAYA_EXPORT
 #include "stats_manager.h"
-#endif
-
-#if defined(USE_DX10) || defined(USE_DX11)
+#include <d3d11.h>
 
 enum ViewPort;
 
@@ -24,23 +19,12 @@ struct HWViewPortRTZB
 		baseRT = nullptr;
 		baseZB = nullptr;
 	};
-#if defined(USE_DX11)
 	ID3D11RenderTargetView* baseRT;	//	combine with DX9 pBaseRT via typedef
 	ID3D11DepthStencilView* baseZB;
-#elif defined(USE_DX10)
-	ID3D10RenderTargetView* baseRT;	//	combine with DX9 pBaseRT via typedef
-	ID3D10DepthStencilView* baseZB;
-#endif	//	USE_DX10
 };
 
-#endif	//
 
-
-class  CHW
-#if defined(USE_DX10) || defined(USE_DX11)
-	:	public pureAppActivate, 
-		public pureAppDeactivate
-#endif	//	USE_DX10
+class  CHW	:	public pureAppActivate, public pureAppDeactivate
 {
 //	Functions section
 public:
@@ -54,9 +38,7 @@ public:
 	void					DestroyDevice			();
 
 	void					Reset					(HWND hw);
-#if defined(USE_DX10) || defined(USE_DX11)
 	void					SwitchVP(ViewPort vp);
-#endif	//	USE_DX10
 
 	void					selectResolution		(u32 &dwWidth, u32 &dwHeight, BOOL bWindowed);
 	D3DFORMAT				selectDepthStencil		(D3DFORMAT);
@@ -66,26 +48,14 @@ public:
 	void					updateWindowProps		(HWND hw);
 	BOOL					support					(D3DFORMAT fmt, DWORD type, DWORD usage);
 
-#ifdef DEBUG
-#if defined(USE_DX10) || defined(USE_DX11)
-	void	Validate(void)	{};
-#else	//	USE_DX10
-	void	Validate(void)	{	VERIFY(pDevice); VERIFY(pD3D); };
-#endif	//	USE_DX10
-#else
-	void	Validate(void)	{};
-#endif
+	void					Validate(void)	{};
 
 //#include <dxgi1_2.h>
 
-//	Variables section
-#if defined(USE_DX10) || defined(USE_DX11)
 public:
 	ViewPort				storedVP;
 	xr_map<ViewPort, HWViewPortRTZB> viewPortsRTZB;
-#endif
 
-#if defined(USE_DX11)	//	USE_DX10
 public:
 	IDXGIAdapter*			m_pAdapter;	//	pD3D equivalent
 	ID3D11Device*			pDevice;	//	combine with DX9 pDevice via typedef
@@ -102,52 +72,13 @@ public:
 	//DXGI_SWAP_CHAIN_DESC1	m_ChainDesc1;	//	DevPP equivalent
 	bool					m_bUsePerfhud;
 	D3D_FEATURE_LEVEL		FeatureLevel;
-#elif defined(USE_DX10)
-public:
-	IDXGIAdapter*			m_pAdapter;	//	pD3D equivalent
-	ID3D10Device1*       	pDevice1;	//	combine with DX9 pDevice via typedef
-	ID3D10Device*        	pDevice;	//	combine with DX9 pDevice via typedef
-	ID3D10Device1*       	pContext1;	//	combine with DX9 pDevice via typedef
-	ID3D10Device*        	pContext;	//	combine with DX9 pDevice via typedef
-	IDXGISwapChain*         m_pSwapChain;
-	ID3D10RenderTargetView*	pBaseRT;	//	combine with DX9 pBaseRT via typedef
-	ID3D10DepthStencilView*	pBaseZB;
-
-	CHWCaps					Caps;
-
-	D3D10_DRIVER_TYPE		m_DriverType;	//	DevT equivalent
-	DXGI_SWAP_CHAIN_DESC	m_ChainDesc;	//	DevPP equivalent
-	bool					m_bUsePerfhud;
-	D3D_FEATURE_LEVEL		FeatureLevel;
-#else
-private:
-	HINSTANCE 				hD3D;
-
-public:
-
-	IDirect3D9* 			pD3D;		// D3D
-	IDirect3DDevice9*		pDevice;	// render device
-
-	IDirect3DSurface9*		pBaseRT;
-	IDirect3DSurface9*		pBaseZB;
-
-	CHWCaps					Caps;
-
-	UINT					DevAdapter;
-	D3DDEVTYPE				DevT;
-	D3DPRESENT_PARAMETERS	DevPP;
-#endif	//	USE_DX10
-
-#ifndef _MAYA_EXPORT
 	stats_manager			stats_manager;
-#endif
-#if defined(USE_DX10) || defined(USE_DX11)
+
 	void			UpdateViews();
 	DXGI_RATIONAL	selectRefresh(u32 dwWidth, u32 dwHeight, DXGI_FORMAT fmt);
 
 	virtual	void	OnAppActivate();
 	virtual void	OnAppDeactivate();
-#endif	//	USE_DX10
 
 private:
 	bool					m_move_window;
